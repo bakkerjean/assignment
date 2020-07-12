2823fbcfdfefcedbbfccf60871bab5a1921b2dae
Jean-Michel Bakker <jmb@Jean-Michels-MacBook-Pro.local>
2020-07-12 13:46:45 +0200

    added basic login setup

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth/auth.service';
+
+import { Creds } from '../../types/creds';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +11,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(public authService: AuthService, public router: Router) {}
   title = 'Login';
+
+  login(creds: Creds) {
+    this.authService.login(creds).subscribe({
+      next: () => {
+        this.authService.setUser({
+          firstName: creds.firstName,
+          lastName: creds.lastName,
+          email: creds.email,
+        });
+        this.router.navigate(['']);
+      },
+      error: (error) => console.log(error),
+    });
+  }
 }
