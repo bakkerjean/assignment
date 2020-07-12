12628082863be6c2ca06faa67275446ab00e8552
Jean-Michel Bakker <jmb@Jean-Michels-MacBook-Pro.local>
2020-07-12 14:14:26 +0200

    fix tests

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,4 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AuthGuardService as AuthGuard } from '../../services/auth/auth-guard.service';
 
 import { LoginComponent } from './login.component';
 
@@ -8,9 +12,10 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent],
+      imports: [RouterTestingModule, HttpClientModule],
+      providers: [AuthGuard],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/services/auth/auth-guard.service.spec.ts
+++ src/app/services/auth/auth-guard.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AuthGuardService } from './auth-guard.service';
 
@@ -6,7 +8,10 @@ describe('AuthGuardService', () => {
   let service: AuthGuardService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
+      providers: [AuthGuardService],
+    });
     service = TestBed.inject(AuthGuardService);
   });
 
