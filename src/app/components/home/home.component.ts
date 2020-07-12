c45fd5efff39a8aa010cbf3ef835028f28d3881a
Jean-Michel Bakker <jmb@Jean-Michels-MacBook-Pro.local>
2020-07-12 19:00:59 +0200

    show user name on home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { User } from 'src/app/types/user';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  user: User;
 
   ngOnInit(): void {
+    this.user = this.authService.getUser();
   }
-
 }
