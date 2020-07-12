1aa62f03af91e4de30f304a5e244971d37832b80
Jean-Michel Bakker <jmb@Jean-Michels-MacBook-Pro.local>
2020-07-12 13:50:36 +0200

    change fallback route to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { LoginComponent } from './components/login/login.component';
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-  { path: '**', redirectTo: 'login' },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
